2497ddbbd60d167d9defcf74c11149961e1e2efc
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2025-10-07 19:14:56 +0100

    created payment form UI

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -1,7 +1,137 @@
-import React from "react";
+import { PaymentFormInputs, paymentFormSchema } from "@/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ShoppingCart } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { SubmitHandler, useForm } from "react-hook-form";
+import Image from "next/image";
 
 const PaymentForm = () => {
-  return <div>PaymentForm</div>;
-};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PaymentFormInputs>({
+    resolver: zodResolver(paymentFormSchema),
+  });
 
+  const router = useRouter();
+
+  const handlePaymentForm: SubmitHandler<PaymentFormInputs> = (data) => {};
+
+  return (
+    <form
+      className="flex flex-col gap-4"
+      onSubmit={handleSubmit(handlePaymentForm)}
+    >
+      {/* CARD HOLDER NAME */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="cardHolder"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Name on card
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cardHolder"
+          placeholder="Rogger Rabbit"
+          {...register("cardHolder")}
+        />
+        {errors.cardHolder && (
+          <p className="text-xs text-red-500">{errors.cardHolder.message}</p>
+        )}
+      </div>
+
+      {/* CARD NUMBER  */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="cardNumber"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Card Number
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cardNumber"
+          placeholder="222-333-444"
+          {...register("cardNumber")}
+        />
+        {errors.cardNumber && (
+          <p className="text-xs text-red-500">{errors.cardNumber.message}</p>
+        )}
+      </div>
+
+      {/* EXPIRATION DATE */}
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="expirationDate"
+          className="text-xs text-gray-500 font-medium"
+        >
+          Expiration Date
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="expirationDate"
+          placeholder="01/27"
+          {...register("expirationDate")}
+        />
+        {errors.expirationDate && (
+          <p className="text-xs text-red-500">
+            {errors.expirationDate.message}
+          </p>
+        )}
+      </div>
+
+      {/* CVV */}
+      <div className="flex flex-col gap-1">
+        <label htmlFor="cvv" className="text-xs text-gray-500 font-medium">
+          CVV
+        </label>
+        <input
+          className="border-b border-gray-200 py-2 outline-none text-sm"
+          type="text"
+          id="cvv"
+          placeholder="123"
+          {...register("cvv")}
+        />
+        {errors.cvv && (
+          <p className="text-xs text-red-500">{errors.cvv.message}</p>
+        )}
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <Image
+          src="/klarna.png"
+          alt="klarna"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+        <Image
+          src="/cards.png"
+          alt="klarna"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+        <Image
+          src="/stripe.png"
+          alt="klarna"
+          width={50}
+          height={25}
+          className="rounded-md"
+        />
+      </div>
+
+      <button
+        className="w-full bg-gray-800 hover:bg-gray-900 transition-all text-white p-2 rounded-lg cursor-pointer flex justify-center items-center gap-2"
+        type="submit"
+      >
+        Checkout <ShoppingCart className="w-3 h-3" />
+      </button>
+    </form>
+  );
+};
 export default PaymentForm;
