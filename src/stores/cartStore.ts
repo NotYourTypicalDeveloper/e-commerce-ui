838c7532156342f63ea1e2d20edaa3d654d0b5b7
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2025-10-08 17:51:04 +0100

    Fixed cart item counting, added property to check if hydration happened

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -6,6 +6,7 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
   persist(
     (set) => ({
       cart: [],
+      hasHydrated: false,
       addToCart: (product) =>
         set((state) => {
           const existingIndex = state.cart.findIndex(
@@ -38,7 +39,15 @@ const useCartStore = create<CartStoreStateType & CartStoreActionsType>()(
       clearCart: () => set({ cart: [] }),
     }),
 
-    { name: "cart", storage: createJSONStorage(() => localStorage) }
+    {
+      name: "cart",
+      storage: createJSONStorage(() => localStorage),
+      onRehydrateStorage: () => (state) => {
+        if (state) {
+          state.hasHydrated = true;
+        }
+      },
+    }
   )
 );
 
